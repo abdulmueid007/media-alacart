d6be09ba8f872adfe29203168dcbce3e20ef6759
Abdul-Hajeeb <abdulmueid007@gmail.com>
2026-04-22 22:13:35 +0530

    added loader for hero image

--- src/app/shared/sections/hero/hero.ts
+++ src/app/shared/sections/hero/hero.ts
@@ -4,15 +4,22 @@ import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
 import { IconComponent } from '../../ui/icon-component/icon-component';
 import { HeroBanner } from '../../../core/model/home';
+import { CommonModule } from '@angular/common';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [AppButton, IconComponent],
+  imports: [AppButton, IconComponent, CommonModule, NgxSkeletonLoaderModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
 export class Hero {
   faPlayCircle = faPlayCircle;
+  imageLoaded = false;
   @Input({ required: true }) heroBanner!: HeroBanner;
+
+  onImageLoad() {
+    this.imageLoaded = true;
+  }
 }
